074e0aa7aeab765a408c08fca70ee15664adac9b
Pierre-yves.baloche <baloche.pierreyves@gmail.com>
2023-08-23 22:28:42 +0200

    Modified version of the initial project to rely on Github's commits management ids to identify version changes

--- test_ota.py
+++ test_ota.py
@@ -1,8 +1,8 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://raw.githubusercontent.com/kevinmcaleer/ota_test/"
+firmware_url = "https://raw.githubusercontent.com/pierreyvesbaloche/kevinmca_ota/main/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test_ota.py")
 
 ota_updater.download_and_install_update_if_available()
